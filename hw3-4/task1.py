79c903664e02a273c54ca8228f71621dc902159f
olex <olexandr.samus.2004@gmail.com>
2023-10-24 13:53:52 +0300

    add hw5

--- hw3-4/task1.py
+++ hw3-4/task1.py
@@ -4,5 +4,12 @@
 
 message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
 search = "r"
-result = 0
-for
+result1 = message.count('r')
+print(result1)
+result2 = 0
+for ch in message:
+    if ch == search:
+        result2 += 1
+
+
+print(result2)
